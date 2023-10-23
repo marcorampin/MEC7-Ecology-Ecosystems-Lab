a5691d28e67db902f20f2a3daa97583eb8d496c7
Marco Rampin <rampin.marco.17@gmail.com>
2023-10-24 00:13:35 +0200

    Added functionality to navbar

--- javascript/script.js
+++ javascript/script.js
@@ -1,32 +1,47 @@
+//is loaded as soon as the website is ready
 $(document).ready(function() {
 
+	let html = $('html');
+	
+	//retrieves the value of a css variable contained in an element
+	const getStyleVariable = (element, varName) => {
+		let computedStyle = window.getComputedStyle(element[0]);
+		let value = computedStyle.getPropertyValue(varName);
+		return value;
+	}
+
+	//scrolls with an animation to the internal link clicked in the nav bar
 	$("nav a").on('click',function(){
-   	var link = $(this).attr('href');
-   	$('html,body').animate({scrollTop: ($(link).offset().top - 100)},'slow');
+   	let link = $(this).attr('href');
+   	let navHeight = getStyleVariable(html, '--navHeight');
+   	//keeps only the number of the variable since it's needed only as a number for the scroll
+   	let numNavHeight = parseFloat(navHeight);
+   	
+   	$('html,body').animate({scrollTop: ($(link).offset().top - (numNavHeight*8))},'slow');
+   	
+   	//It hides again all the links once a link has been clicked
+   	let x = document.getElementById("myLinks");
+   	x.style.display = "none";
+   	
    	return false;
 	});
-
+	
+	//resize the height of the window height considering also the browsers components (needed for smartphones)
     const resizeHeight = () => {
-		$('html').css('--maxHeight', window.innerHeight+'px');
+		html.css('--maxHeight', window.innerHeight+'px');
 	}
-    
-    const loadFunctions = () => {
-		resizeHeight();
-	}
-
    window.addEventListener("resize", resizeHeight);
-  	window.addEventListener('load', loadFunctions());
 });
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon 
-See online if it's possible to add a Event listener to see the click*/
-/*
+See online if it's possible to add a Event listener to see the click and if it's possible to make it in jQuery*/
+
 function myFunction() {
-  var x = document.getElementById("myLinks");
+  let x = document.getElementById("myLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
   }
 }
-*/
+
